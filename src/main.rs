378c8f543cc674db72e745db59e03dd903db7928
Adam Weinberger <adamw@adamw.org>
2023-08-08 10:16:36 -0400

    Show memory percent too

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,40 @@ fn main() {
         .with_memory();
     let mut sys = System::new_with_specifics(refresh);
 
+    let mem_total = sys.total_memory();
+
     // Sleep for 1 second to get an initial reading
     sleep(Duration::from_secs(1));
 
     loop {
         sys.refresh_specifics(refresh);
 
-        let cpu = sys.global_cpu_info().cpu_usage().round();
-        let mem = to_gb(sys.used_memory());
+        let cpu = sys.global_cpu_info().cpu_usage();
+        let cpu_percent = fmt_cpu_percent(cpu);
 
-        println!("{cpu}% {mem}G");
+        let mem_used = sys.used_memory();
+        let mem_gb = fmt_memory_gb(mem_used);
+        let mem_percent = fmt_memory_percent(mem_used, mem_total);
+
+        println!("{cpu_percent} {mem_percent}({mem_gb})");
         sleep(REFRESH_TIME);
     }
+}
+
+fn fmt_cpu_percent(cpu: f32) -> String {
+    let percent = cpu.round();
+
+    format!("{percent}%")
+}
+
+fn fmt_memory_gb(mem_used: u64) -> String {
+    let mem_bytes = to_gb(mem_used);
+    format!("{mem_bytes}G")
+}
+
+fn fmt_memory_percent(mem_used: u64, mem_total: u64) -> String {
+    let mem_percent = ((mem_used as f64 / mem_total as f64) * 100.0).round();
+    format!("{mem_percent}%")
 }
 
 fn to_gb(bytes: u64) -> f64 {
